65ff6853f1ba1abe8c80ad220eb1f0116855d27d
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-04-03 10:41:47 +0700

    add dev deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,42 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import tailwindcssNesting from "tailwindcss/nesting";
+import postcssImport from "postcss-import";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  prefetch: true,
+  adapter: node({
+    mode: "middleware",
+  }),
+  integrations: [
+    react({
+      experimentalReactChildren: true,
+    }),
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  vite: {
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: "http://localhost:3001",
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting(), postcssImport()],
+      },
+    },
+  },
 });
