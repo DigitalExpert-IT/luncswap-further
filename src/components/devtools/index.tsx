8e05181b2f3b047e901ee02b648b426dc34b71ad
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-04-26 18:47:07 +0700

    init page

--- src/components/devtools/index.tsx
+++ src/components/devtools/index.tsx
@@ -1,9 +1,22 @@
 import { useEffect } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Devtools() {
   useEffect(() => {}, []);
 
-  return null;
+  return (
+    <Tabs defaultValue="instantiate" className="w-[400px] mx-auto">
+      <h1 className="text-2xl font-bold text-center my-6">Devtools</h1>
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="instantiate">Instantiate</TabsTrigger>
+        <TabsTrigger value="store-code">Upload Wasm</TabsTrigger>
+      </TabsList>
+      <TabsContent value="instantiate">
+        Make changes to your account here.
+      </TabsContent>
+      <TabsContent value="store-code">Change your password here.</TabsContent>
+    </Tabs>
+  );
 }
 
 export default Devtools;
