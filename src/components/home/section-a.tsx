aa5ab4455844ad08ed82cc718bb6c9ba8b30cd82
Y0SH4 <yosa.agung13@gmail.com>
2024-05-06 20:49:34 +0700

    reafactor: landing page section-a

--- src/components/home/section-a.tsx
+++ src/components/home/section-a.tsx
@@ -2,29 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export function SectionA() {
   return (
-    <div className="section-a">
-      <div className="inner">
-        <div className="heading">
-          <h1>
-            A DEX exclusively created for{" "}
-            <span className="punch">LUNA CLASSIC $LUNC</span>
-          </h1>
-          <p>
-            Every Swap Is a Step Towards Higher LUNC Value. Swap Now, Burn 1.5%,
-            and Watch Our LUNC Reach New Heights.
-          </p>
-          <div>
-            <Button className="text-lg" size="lg">
-              Launch app
-            </Button>
-          </div>
-        </div>
-        <img
-          className="illustration"
-          src="https://ik.imagekit.io/msxxxaegj/Luncswap/rabbit-bull.png?updatedAt=1712160475269"
-          alt="bull-rabit"
-        />
-      </div>
+    <div className="relative bg-sky-900">
+      <img
+        src="https://ik.imagekit.io/msxxxaegj/Luncswap/jering.png?updatedAt=1715000183520"
+        className="absolute"
+      />
     </div>
   );
 }
