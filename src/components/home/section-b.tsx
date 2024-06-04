8cf0e67303e21ba09fcd875ed6cc4cffb29557d0
Y0SH4 <yosa.agung13@gmail.com>
2024-06-04 22:33:12 +0700

    update

--- src/components/home/section-b.tsx
+++ src/components/home/section-b.tsx
@@ -1,21 +1,37 @@
-export function SectionB() {
+import { Button } from "../ui/button";
+
+type CardType = {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+};
+
+type SectionBType = {
+  data: CardType[];
+};
+
+export function SectionB({ data }: SectionBType) {
   return (
     // view
     <div className="section-b container">
       <div className="flex space-x-10">
-        <div className="overflow-hidden border-2 rounded-xl max-h-100">
-          <img
-            src="/burn-corp.jpg"
-            alt="burn"
-            className="object-cover w-full h-full"
-          />
-        </div>
-        <div className="overflow-hidden border-2 rounded-xl">
-          <img src="/burn-corp.jpg" alt="burn" className="" />
-        </div>
-        <div className="overflow-hidden border-2 rounded-xl">
-          <img src="/burn-corp.jpg" alt="burn" className="" />
-        </div>
+        {data.map((item, idx) => (
+          <div className="overflow-hidden border-2 rounded-2xl" key={idx}>
+            <img
+              src={item.image}
+              alt="burn"
+              className="object-cover w-full h-full z-0"
+            />
+            <div className="bg-black z-10">
+              <h1>{item.title}</h1>
+              <span>{item.description}</span>
+              <a href={item.url}>
+                <Button>Burn</Button>
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/constant/dummy.ts
@@ -0,0 +1,19 @@
+export const CARD = [
+  {
+    title: "1.5% Burn",
+    description: "1.5% burn tax, boosting LUNC's value",
+    image: "/burn-crop.jpg",
+    url: "#",
+  },
+  {
+    title: "stacking",
+    description: "stake with LUNC SWAP Validator",
+    image: "/stacking.jpg",
+    url: "#",
+  },
+  {
+    title: "launchpad",
+    description: "with every LUNC Burn, Your Project's Value Ignites",
+    image: "/launchpad.jpg",
+  },
+];
